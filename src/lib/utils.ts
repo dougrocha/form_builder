5d6086703dd3c53c63ff09a6c20aa7a00f9fd1f0
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2025-04-16 01:48:45 -0700

    feat(utils): added try catch to better handle try catches

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function tryCatch<T, E = Error>(
+  fn: () => Promise<T> | T,
+): Promise<[T | null, E | null]> {
+  try {
+    const result = await fn();
+    return [result, null];
+  } catch (e: unknown) {
+    return [null, e as E];
+  }
+}
